813c43dd550a8ce9944bd943446d1b2861118a32
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2017-09-05 20:23:23 +0300

    стилистические изменения

--- SpringSecExample/Sample_01/src/main/java/com/journaldev/spring/controller/HomeController.java
+++ SpringSecExample/Sample_01/src/main/java/com/journaldev/spring/controller/HomeController.java
@@ -31,7 +31,20 @@ public class HomeController {
 		
 		return "home";
 	}
-	
+
+	@RequestMapping(value = "/emp/home", method = RequestMethod.GET)
+	public String home2(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+		String formattedDate = dateFormat.format(date);
+
+		model.addAttribute("serverTime", formattedDate );
+
+		return "home";
+	}
+
 	@RequestMapping(value = "/emp/get/{id}", method = RequestMethod.GET)
 	public String getEmployee(Locale locale, Model model,@PathVariable("id") int id) {
 		logger.info("Welcome user! Requested Emp ID is: "+id);
