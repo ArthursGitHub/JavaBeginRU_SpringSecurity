2c30eaf0e186c75b755e0c09d1ce8085aaed27eb
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2017-10-06 01:36:52 +0300

    маленький фикс

--- Security_22/src/main/java/ru/javabegin/training/services/ProcessInterface.java
+++
@@ -1,9 +0,0 @@
-package ru.javabegin.training.services;
-
-import org.springframework.security.access.annotation.Secured;
-
-public interface ProcessInterface {
-
-    @Secured("ROLE_ADMIN")
-    String getMessage();
-}
